fba6a6fd76adeb50e0408072309935e4796ebecf
AndreaCirilloAC <andreacirilloac@gmail.com>
2015-07-27 17:51:08 +0200

    little formatting changes

--- R/ramazon_mac.R
+++ R/ramazon_mac.R
@@ -8,20 +8,20 @@ ramazon <- function( Public_DNS = "", key_pair_name = ""){
 
 #set useful variables
 cd = getwd()
-key_pair_address = paste(cd,key_pair_name,"pem", sep ="")
-user_server = paste(key_pair_address,"ubuntu@",Public_DNS, sep ="")
-command = paste("chmod 400 ",key_pair_address,sep ="")
+key_pair_address = paste(cd,key_pair_name,".pem", sep = "")
+user_server = paste(key_pair_address,"ubuntu@",Public_DNS, sep = "")
+command = paste("chmod 400 ",key_pair_address,sep = "")
 system(command,intern = TRUE)
 
 # establish a connection with amazon AWS instance
-command = paste("ssh -i -v ",user_server,sep ="")
+command = paste("ssh -i -v ",user_server,sep = "")
 system(command, intern = TRUE)
 
 # modify sources.list file
 system("sudo apt-get update", intern = TRUE)
 system("sudo apt-get upgrade", intern = TRUE)
 system("echo 'deb https://cran.rstudio.com/bin/linux/ubuntu trusty/' >> /etc/apt/sources.list-new", intern = TRUE)
-system("mv /etc/apt/sources.list-new /etc/apt/sources.list",intern =TRUE)
+system("mv /etc/apt/sources.list-new /etc/apt/sources.list",intern = TRUE)
 
 # install latest R version
 system("sudo apt-get install -y r-base", intern = TRUE)
@@ -30,7 +30,7 @@ system("sudo apt-get install -y r-base", intern = TRUE)
 
 # install latest Shiny server version
 system("sudo apt-get install gdebi-core", intern = TRUE)
-system("wget http://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.3.0.403-amd64.deb", intern =TRUE)
+system("wget http://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.3.0.403-amd64.deb", intern = TRUE)
 system("sudo gdebi shiny-server-1.3.0.403-amd64.deb", intern = TRUE)
 
 # delete standard example
@@ -44,10 +44,10 @@ files = list.files(getwd()) # list file within the current directory ( subdirect
 for (i in 1:length(files)){ # loop files to copy file into the server instance
 from_address = paste( getwd(),files = i,sep = "/")
 to_address   = paste( user_server,"srv/shiny-server",files = i,sep = "/")
-command      = paste("scp",from_address,to_address,sep =" ")
+command      = paste("scp",from_address,to_address,sep = " ")
 }
 
 # navigate the app in a browser
-app_url = paste(Public_DNS,":3838",sep ="")
+app_url = paste(Public_DNS,":3838",sep = "")
 browseURL(app_url)
 }
