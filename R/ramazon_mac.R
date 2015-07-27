23196bf34e42250b2077e0303135d622ca0e9545
Andrea Cirillo <andreacirilloac@gmail.com>
2015-07-27 16:27:23 +0200

    written for loop to copy files into server

--- R/ramazon_mac.R
+++ R/ramazon_mac.R
@@ -2,17 +2,20 @@ ramazon( Public_DNS = "", key_pair_name = ""){
 
 # establish a connection with amazon AWS instance
 
-key_pair_address = paste(getwd(),key_pair_name,"pem", sep ="")
+cd = getwd()
+key_pair_address = paste(cd,key_pair_name,"pem", sep ="")
+user_server = paste(key_pair_address,"ubuntu@",Public_DNS, sep ="")
+
 command = paste("chmod 400 ",key_pair_address,sep ="")
 system(command,intern = TRUE)
-command = paste("ssh -i -v ",key_pair_address,"ubuntu@",Public_DNS,sep ="")
+command = paste("ssh -i -v ",user_server,sep ="")
 system(command, intern = TRUE)
 
 # modify sources.list file
 system("sudo apt-get update", intern = TRUE)
 system("sudo apt-get upgrade", intern = TRUE)
 system("sudo nano /etc/apt/sources.list", intern = TRUE)
-# MISSING CODE
+# MISSING CODE (append text to sources.list)
 # install latest R version
 system("sudo apt-get install -y r-base", intern = TRUE)
 
@@ -25,9 +28,19 @@ system("wget http://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.3.0
 system("sudo gdebi shiny-server-1.3.0.403-amd64.deb", intern = TRUE)
 
 # delete standard example
+system("rm -Rf /srv/shiny-server/example.R", intern = TRUE)
+system("rm -Rf /srv/shiny-server/example.R", intern = TRUE)
+system("rm -Rf /srv/shiny-server/example.R", intern = TRUE)
+# MISSING CODE(specify files to be removed)
 
 # paste shiny app files
 
+files = list.files(getwd()) # list file within the current directory ( subdirectories not included)
+for (i in 1:length(files)){ # loop files to copy file into the server instance
+from_address = paste( getwd(),files = i,sep = "/")
+to_address   = paste( user_server,"srv/shiny-server",files = i,sep = "/")
+command      = paste("scp",from_address,to_address,sep =" ")
+}
 # navigate the app in a browser
 
 }
